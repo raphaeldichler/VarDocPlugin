393ad0976deb0795ca6e895dbe2906cab6144216
apocalypse14 <kagandurmu2001@gmail.com>
2024-11-23 11:14:20 +0100

    Selection possible

--- src/main/java/de/tum/vardocplugin/DropdownTableAction.java
+++ src/main/java/de/tum/vardocplugin/DropdownTableAction.java
@@ -3,13 +3,12 @@ package de.tum.vardocplugin;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.table.JBTable;
 
-import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import com.intellij.ui.components.JBScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class DropdownTableAction extends AnAction {
     @Override
@@ -27,30 +26,33 @@ public class DropdownTableAction extends AnAction {
         // Create the JBTable with the TableModel
         JBTable table = new JBTable(tableModel, null);
         table.setAutoResizeMode(JBTable.AUTO_RESIZE_ALL_COLUMNS); // Optional: Resize columns to fit
+        table.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION); // Single row selection
 
-        // Add a mouse listener for row click
-        table.addMouseListener(new MouseAdapter() {
+        // Add a ListSelectionListener to handle selection changes
+        table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             @Override
-            public void mouseClicked(MouseEvent event) {
-                int selectedRow = table.getSelectedRow();
-                if (selectedRow != -1) {
-                    String selectedName = table.getValueAt(selectedRow, 1).toString();
-                    JOptionPane.showMessageDialog(null, "You selected: " + selectedName);
+            public void valueChanged(ListSelectionEvent event) {
+                if (!event.getValueIsAdjusting()) {
+                    int selectedRow = table.getSelectedRow();
                 }
             }
         });
+
         // Wrap the JBTable in a JBScrollPane
         JBScrollPane scrollPane = new JBScrollPane(table);
 
-
-        // Display the table in a popup
+        // Create and show the popup with focus on the table
         JBPopupFactory.getInstance()
                 .createComponentPopupBuilder(scrollPane, null)
                 .setTitle("Employee Details")
+                .setFocusable(true) // Allow focus
+                .setRequestFocus(true) // Request focus for keyboard navigation
                 .setResizable(true)
                 .setMovable(true)
                 .createPopup()
                 .showInFocusCenter();
+
+        // Request focus for the table explicitly
+        table.requestFocusInWindow();
     }
 }
-
